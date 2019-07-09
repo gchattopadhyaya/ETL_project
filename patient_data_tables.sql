5f668d679a3e3d5e368826eaedefd63acf0566ef
Lisa Naser <lmkn@Johns-MacBook-Air.local>
2019-07-08 20:16:17 -0700

    populated tables successfully in SQL

--- patient_data_tables.sql
+++ patient_data_tables.sql
@@ -1,2 +1,19 @@
-CREATE TABLE patient_data
+CREATE TABLE patient_data (
+	PatientID INT,
+	Treatment VARCHAR,
+	SampleDay VARCHAR,
+	AnalysisBatch INT, 
+	QualityControl VARCHAR,
+	CellType VARCHAR,
+	PerecntCells NUMERIC,
+	CellCount INT
+);
+
+CREATE TABLE patient_key (
+	PatientID INT primary key,
+	PatientSex VARCHAR,
+	PatientAge INT, 
+	OnSteroids VARCHAR,
+	PatientEthnicity VARCHAR
+);
 	
