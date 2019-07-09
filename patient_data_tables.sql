57699101211e7130995925150c371b1df8b4c0d6
Lisa Naser <lmkn@Johns-MacBook-Air.local>
2019-07-08 20:27:15 -0700

    merged tables made in SQL and pandas

--- patient_data_tables.sql
+++ patient_data_tables.sql
@@ -1,19 +1,31 @@
 CREATE TABLE patient_data (
-	PatientID INT,
-	Treatment VARCHAR,
-	SampleDay VARCHAR,
-	AnalysisBatch INT, 
-	QualityControl VARCHAR,
-	CellType VARCHAR,
-	PerecntCells NUMERIC,
-	CellCount INT
+	patientid INT,
+	treatment VARCHAR,
+	sampleday VARCHAR,
+	analysisbatch INT, 
+	qualitycontrol VARCHAR,
+	celltype VARCHAR,
+	perecentcells NUMERIC,
+	cellcount INT
 );
 
+DROP TABLE patient_data;
+
 CREATE TABLE patient_key (
-	PatientID INT primary key,
-	PatientSex VARCHAR,
-	PatientAge INT, 
-	OnSteroids VARCHAR,
-	PatientEthnicity VARCHAR
+	patientid INT primary key,
+	patientsex VARCHAR,
+	patientage INT, 
+	onsteroids VARCHAR,
+	patientethnicity VARCHAR
 );
+
+DROP TABLE patient_key;
 	
+-- test that data was loaded correctly from pandas. learned that postgres cant take the camelcase from pandas
+SELECT * FROM patient_data;	
+SELECT * FROM patient_key;	
+
+-- example of joining tables in SQL instead of in pandas
+SELECT * FROM patient_data
+JOIN patient_key
+ON patient_data.patientid = patient_key.patientid;
