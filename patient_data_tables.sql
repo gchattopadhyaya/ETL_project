3edcd96de93586a31207f3d862aa23dee1e7d7df
Lisa Naser <lmkn@Johns-MacBook-Air.local>
2019-07-10 19:01:56 -0700

    final commit for project requirements

--- patient_data_tables.sql
+++ patient_data_tables.sql
@@ -29,3 +29,23 @@ SELECT * FROM patient_key;
 SELECT * FROM patient_data
 JOIN patient_key
 ON patient_data.patientid = patient_key.patientid;
+
+--we also did the merge in pandas to show how to do it there and the following is the table of merged data generated in pandas and populated here:
+CREATE TABLE merged_data (
+	patientid INT,
+	treatment VARCHAR,
+	sampleday VARCHAR,
+	analysisbatch INT, 
+	qualitycontrol VARCHAR,
+	celltype VARCHAR,
+	perecentcells NUMERIC,
+	cellcount INT,
+	patientsex VARCHAR,
+	patientage INT, 
+	onsteroids VARCHAR,
+	patientethnicity VARCHAR
+);
+
+--test that data loaded to merged_data table
+SELECT * FROM merged_data;
+--success!
